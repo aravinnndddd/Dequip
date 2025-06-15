02f4ebe526c0a1bca478e3ede1f40ed061959d8f
aravinnndddd <aromalaravind0@gmail.com>
2025-06-15 21:01:52 +0530

    Update mainContent.jsx

--- src/components/mainContent.jsx
+++ src/components/mainContent.jsx
@@ -5,7 +5,7 @@ import vdo from "../assets/vdo.mov";
 import poster from "../assets/poster2.png";
 import logo1 from "../assets/logo1.png";
 function MainContent() {
-  const [time, setTime] = useState({ hours: 0, minutes: 4, seconds: 8 });
+  const [time, setTime] = useState({ hours: 0, minutes: 5, seconds: 45 });
   const [isTimerComplete, setIsTimerComplete] = useState(false);
 
   useEffect(() => {
