ad8685ff3dee8916aacb4144a1050e65fe3f072c
Aravind <aromalaravind0@gmail.com>
2025-06-17 10:17:34 +0530

    added new video and change heading

--- src/components/mainContent.jsx
+++ src/components/mainContent.jsx
@@ -11,7 +11,11 @@ function MainContent() {
   useEffect(() => {
     const timer = setInterval(() => {
       setTime((prevTime) => {
-        if (prevTime.hours === 0 && prevTime.minutes === 0 && prevTime.seconds === 0) {
+        if (
+          prevTime.hours === 0 &&
+          prevTime.minutes === 0 &&
+          prevTime.seconds === 0
+        ) {
           clearInterval(timer);
           setIsTimerComplete(true);
           return prevTime;
@@ -39,67 +43,79 @@ function MainContent() {
 
   return (
     <>
-    
-    <div className="app-container">
-      <div className="container">
-    <center><div className="logorn"><img src={logo1} alt="logo"  width={65}/></div></center>
-        {/* Main Content */}
-        <div>
-          <h2 className="subtitle">Limited Time Training Reveals:</h2>
-          <h1 className="main-title">
-
-          
-
-          How I Made <span className="highlight"> ₹324,000+</span> With {" "} 
-            <span className="highlight">Automated </span> Instagram CashPages 
-          </h1>
-          <p className="subtitle " id="doThe" ><b>And How You Can Do The SAME!</b></p>
-          <p className="subtitle" id="sub2" >Watch The <b>Complete</b> Video Below</p>
-        </div>
-
-        {/* Video Container */}
-        <div>
-          <div className="video-container">
-            <video src={vdo}  controls playsInline poster={poster}>
-             
-            </video>
-          </div>
-
-          {/* Timer */}
-          <div className="timer-container">
-            <div className="timer-box">
-              <div className="timer-circle">{String(time.hours).padStart(2, "0")}</div>
-              <p className="timer-label">Hour</p>
+      <div className="app-container">
+        <div className="container">
+          <center>
+            <div className="logorn">
+              <img src={logo1} alt="logo" width={65} />
             </div>
-            <div className="timer-box">
-              <div className="timer-circle">{String(time.minutes).padStart(2, "0")}</div>
-              <p className="timer-label">Minute</p>
-            </div>
-            <div className="timer-box">
-              <div className="timer-circle">{String(time.seconds).padStart(2, "0")}</div>
-              <p className="timer-label">Second</p>
-            </div>
-          </div>
-        </div>
-
-        {/* CTA Button */}
-        {isTimerComplete && (
+          </center>
+          {/* Main Content */}
           <div>
-            <a href="https://superprofile.bio/course/9da35b5f-14ea-4352-9c18-d7a55a3d0e6f?checkout=true" className="cta-button">
-              Buy now for ₹199
-
-            </a>
+            <h2 className="subtitle">Limited Time Training Reveals:</h2>
+            <h1 className="main-title">
+              How to<span className="highlight"> Make Money</span> Using
+              Instagram <span className="highlight"> Without Selling </span>
+              Anything!
+            </h1>
+            <p className="subtitle " id="doThe">
+              <b>And Without Showing Your Face</b>
+            </p>
+            <p className="subtitle" id="sub2">
+              Watch The <b>Complete</b> Video Below
+            </p>
           </div>
-        )}
+
+          {/* Video Container */}
+          <div>
+            <div className="video-container">
+              <video src={vdo} controls playsInline poster={poster}></video>
+            </div>
+
+            {/* Timer */}
+            <div className="timer-container">
+              <div className="timer-box">
+                <div className="timer-circle">
+                  {String(time.hours).padStart(2, "0")}
+                </div>
+                <p className="timer-label">Hour</p>
+              </div>
+              <div className="timer-box">
+                <div className="timer-circle">
+                  {String(time.minutes).padStart(2, "0")}
+                </div>
+                <p className="timer-label">Minute</p>
+              </div>
+              <div className="timer-box">
+                <div className="timer-circle">
+                  {String(time.seconds).padStart(2, "0")}
+                </div>
+                <p className="timer-label">Second</p>
+              </div>
+            </div>
+          </div>
+
+          {/* CTA Button */}
+          {isTimerComplete && (
+            <div>
+              <a
+                href="https://superprofile.bio/course/9da35b5f-14ea-4352-9c18-d7a55a3d0e6f?checkout=true"
+                className="cta-button"
+              >
+                Buy now for ₹199
+              </a>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
-    <footer>This website is not part of the Meta, Instagram, or Facebook. Additionally, this site is not endorsed by Meta, Instagram, or Facebook in any way. Facebook and Instagram are trademarks of Meta Platforms, Inc.</footer>
+      <footer>
+        This website is not part of the Meta, Instagram, or Facebook.
+        Additionally, this site is not endorsed by Meta, Instagram, or Facebook
+        in any way. Facebook and Instagram are trademarks of Meta Platforms,
+        Inc.
+      </footer>
     </>
   );
 }
 
 export default MainContent;
-
-
-
-
