5a7eca53e6146c14dbdff2313f512199c0a41939
Aravind <aromalaravind0@gmail.com>
2025-06-21 20:18:27 +0530

    fix4

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
+// vite.config.js
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: ['**/*.JPG'], // Add this line
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, 'src'),
+    },
+  },
 });
